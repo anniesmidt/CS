d420bf7ece0fef9fa992384e37ccb3686534baf3
Annie Smidt <annie@durablecreative.com>
2014-03-07 15:47:39 -0500

    created html for contact form, modified static styles, added images for bullet and FPO recaptcha

---
+++ html/contact.php
@@ -0,0 +1,54 @@
+<?php include 'header.php'; ?>
+
+<div id="form-page">
+
+
+
+       <h1>Contact Us</h1>
+
+<section class="form-content">
+       
+        <p>For support or more information about CampSeekers, please contact us at 888.467.8202 or use the form below.</p>
+        
+        <form id="contact-form">
+            
+            <label for="name">Name</label>
+            <input type="text">
+            
+            <label for="email">Email</label>
+            <input type="email">
+            
+            <label for="comments">Comments</label>
+            <textarea name="comments" id="comments"></textarea>
+
+            <div class="captcha"><img src="../images/reCAPTCHA_Sample_Clean.png"></div>
+
+            <input type="submit" value="submit" id="submit">
+
+
+        </form>
+            
+
+</section><!--end  content-->   
+
+
+
+<section class ="form-right-sidebar">
+     <p><span class="bold">CampSeekers</span><br>
+        <span class="green">14 Hancock Street<br>
+        Boston, MA 02114<br>
+        888.467.8202</span></p>
+
+           
+</section><!--end details right sidebar-->    
+
+
+
+
+</div><!--end form page-->        
+        
+    
+<?php include 'footer.php'; ?>
+
+    
+    
