945e49225cae98dcfd2a23c8dd59f5afc9a44646
Annie Smidt <annie@durablecreative.com>
2014-03-11 01:09:14 -0400

    created snippet for set favorites styles

---
+++ html/set_favorites.php
@@ -0,0 +1,32 @@
+<!--do not keep-->
+<?php include "header.php"; ?>
+<div class="spacer">.</div>
+<!--end do not keep-->
+
+
+
+
+<div class="star">
+               <a href="#"><img src="../images/bluestar.png"></a>    
+</div><!--end greystar-->
+
+<div class="star-box">
+
+<form>
+    <input type="checkbox" input id="camperA"> Anna </br>
+    <input type="checkbox" input id="camperB"> Lori </br>
+    
+    <button type="button" id="set-favorites">Set</button>
+</form>
+
+</div>
+
+
+
+
+
+
+<!--do not keep-->
+<div class="spacer">.</div>
+<?php include "footer.php"; ?>
+<!--end do not keep-->
